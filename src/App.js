31bfc19770dde9c960addbcb4f8903a89fe615ec
Houtan Rocky <mast9rmind@gmail.com>
2024-03-23 08:50:32 +0300

    Add react and react DOM with typescript

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <div>
+        <h1>My App</h1>
+        <p>It works!</p>
+        <p> This is a simple implementation of React.js in parcel.</p>
+    </div>
+    </>
+  )
+}
